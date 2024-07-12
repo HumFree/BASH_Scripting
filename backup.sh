e1758855a30d658e1223b1ea62ca9a4c52d274bb
Christopher Humphrey <c.r.humphrey2@gmail.com>
2024-07-12 09:23:50 -0400

    A BASH script for backing up files

---
+++ backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Author:
+
+# Script to backup files.
+
+BACKUP_SRC="/tmp"
+BACKUP_DST="/home"
+BACKUP_DATE=$(date '+%F')
+BACKUP_FILENAME="BACKUP_$BACKUP_DATE.tar.gz"
+
+mkdir -p "$BACKUP_DST/$BACKUP_DATE"
+
+# Archive of file
+
+tar -czf "$BACKUP_DST/$BACKUP_DATE/$BACKUP_FILENAME" "$BACKUP_SRC"
+
+# Verify the backup was succesful.
+
+if [ $? -eq 0 ]; then
+	echo "The backup was succesful!"
+else
+	"The backup failed!"
+fi
